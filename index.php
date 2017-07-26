0cb136e28e5a39eac715080816e22a2854091d8f
Rangga Herly Hilman Herawan <rangga.hilmanz@gmail.com>
2017-07-26 15:37:43 +0700

    Initial
    BACKUP Initiation

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>FLUID WIDTH BY ACIT JAZZ</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/flexible-grids.css" rel="stylesheet" type="text/css"/>
+<link href="css/eu-grids.css" rel="stylesheet" type="text/css"/>
+<!--[if lte IE 7]>
+<link href="../yaml/core/iehacks.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+<header>
+	<div class="eu-wrapper">
+		<div class="eu-logo">
+			<a class="logo" href="#" title="logo">Logo Name</a>
+		</div>
+	</div>
+</header>
+<nav id="nav">
+	<div class="eu-wrapper">
+		<div class="eu-hlist">
+			<ul>
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#">Tips Cinta</a></li>
+				<li><a href="#">Kesehatan Cinta</a></li>
+				<li><a href="#">Cerita Cinta</a></li>
+				<li><a href="#">Informasi umum</a></li>
+			</ul>
+			<form class="eu-searchform">
+				<input class="eu-searchfield" type="search" placeholder="Search..." />
+				<input class="eu-searchbutton" type="submit" value="Search" />
+			</form>
+		</div>
+	</div>
+</nav>
+<div id="main">
+	<div class="eu-wrapper">
+		<div class="container">
+			<section class="box info">
+				<div class="eu-grid linearize-level-1">
+					<div class="eu-g66 eu-gl">
+
+						<div class="eu-grid linearize-level-2">
+							<article class="eu-g50 eu-gl">
+								<div class="eu-gbox-left">
+									<h4>Here's a box</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+									<a class="eu-button eu-next" href="#">Read More</a> </div>
+							</article>
+							<article class="eu-g50 eu-gr">
+								<div class="eu-gbox">
+									<h4>And another box</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+									<a class="eu-button eu-next" href="#">Read More</a> </div>
+							</article>
+						</div>
+
+					</div>
+					<article class="eu-g33 eu-gr">
+						<div class="eu-gbox-right secondary">
+							<h4>This box is aligned with the sidebar</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+						</div>
+					</article>
+				</div>
+			</section>
+
+			<section class="eu-grid linearize-level-1">
+				<article class="eu-g66 eu-gl content">
+					<div class="eu-gbox-left eu-clearfix">
+						<img src="http://dummyimage.com/240x160/cccccc/fff.png" alt="" class="float-left bordered"/>
+						<p>Lorem ipsum dolor sit amet, <em>consectetuer adipiscing elit</em>. Nunc congue ipsum vestibulum libero. Aenean vitae justo. Nam eget tellus. Etiam convallis, est eu lobortis mattis, lectus tellus tempus felis, a ultricies erat ipsum at metus.</p>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi et risus. Aliquam nisl. Nulla facilisi. Cras accumsan vestibulum ante. Vestibulum sed tortor. Praesent SMALL CAPS tempus fringilla elit. Ut elit diam, sagittis in, nonummy in, gravida non, nunc. Ut orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Nam egestas, orci eu imperdiet malesuada, nisl purus fringilla odio, quis commodo est orci vitae justo. Aliquam placerat odio tincidunt nulla. Cras in libero. Aenean rutrum, magna non tristique posuere, erat odio eleifend nisl, non convallis est tortor blandit ligula. Nulla id augue.</p>
+						<p>Nullam mattis, odio ut tempus facilisis, metus nisl facilisis metus, auctor consectetuer felis ligula nec mauris. Vestibulum odio erat, fermentum at, commodo vitae, ultrices et, urna. Mauris vulputate, mi pulvinar sagittis condimentum, sem nulla aliquam velit, sed imperdiet mi purus eu magna. Nulla varius metus ut eros. Aenean aliquet magna eget orci. Class aptent taciti sociosqu ad litora.</p>
+						<p>Vivamus euismod. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse vel nibh ut turpis dictum sagittis. Aliquam vel velit a elit auctor sollicitudin. Nam vel dui vel neque lacinia pretium. Quisque nunc erat, venenatis id, volutpat ut, scelerisque sed, diam. Mauris ante. Pellentesque habitant   morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec mattis. Morbi dignissim sollicitudin libero. Nulla lorem.</p>
+						<blockquote>
+							<p>Integer cursus ornare mauris. Praesent nisl arcu, imperdiet eu, ornare id, scelerisque ut, nunc. Praesent sagittis erat sed velit tempus imperdiet. Ut tristique, ante in interdum hendrerit, erat enim faucibus felis, quis rutrum mauris lorem quis sem. Vestibulum ligula nisi, mattis nec, posuere et, blandit eu, ligula. Nam suscipit placerat odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Pellentesque tortor libero, venenatis vitae, rhoncus eu, placerat ut, mi. Nulla nulla.</p>
+						</blockquote>
+						<p>Maecenas vel metus quis magna pharetra fermentum. <em>Integer sit amet tortor</em>. Maecenas porttitor, pede sed gravida auctor, nulla augue aliquet elit, at pretium urna orci ut metus. Aliquam in dolor. Vestibulum ante ipsum   primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, tellus id ornare posuere, quam nunc accumsan turpis, at   convallis tellus orci et nisl. Phasellus congue neque a lorem.</p>
+						<div class="eu-grid linearize-level-2">
+							<div class="eu-g50 eu-gl">
+								<div class="eu-gbox-left">
+									<!-- content -->
+									<h6>This is a nested column</h6>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+								</div>
+							</div>
+							<div class="eu-g50 eu-gr">
+								<div class="eu-gbox-right">
+									<!-- content -->
+									<h6>This is another nested column </h6>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip  ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</article>
+				<aside class="eu-g33 eu-gr">
+					<div class="eu-gbox-right eu-clearfix">
+						<h3>A Simple Sidebar </h3>
+						<p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices  posuere cubilia Curae; Cras ornare mattis nunc. Mauris venenatis, pede  sed aliquet vehicula, lectus tellus pulvinar neque, non cursus sem nisi vel augue.</p>
+						<p>Mauris a lectus. Aliquam erat volutpat. Phasellus ultrices mi a sapien. Nunc rutrum egestas lorem. Duis ac sem sagittis elit tincidunt gravida. Mauris a lectus. Aliquam erat volutpat. Phasellus ultrices mi a sapien. Nunc rutrum egestas lorem. Duis ac sem sagittis elit tincidunt gravida.</p>
+						<p class="box info">Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ornare mattis nunc. Mauris venenatis, pede sed aliquet  vehicula, lectus tellus pulvinar neque, non cursus sem nisi vel augue.</p>
+						<h3>Incremental leading</h3>
+						<p>Vestibulum ante ipsum  primis in faucibus orci luctus vestibulum ante ipsum primis in faucibus  orci luctus et ultrices posuere cubilia Curae; Cras ornare mattis nunc. Mauris venenatis, pede sed aliquet vehicula, lectus tellus pulvinar neque, non cursus sem nisi vel augue. sed aliquet vehicula, lectus  tellus.</p>
+						<p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ornare  mattis nunc. Mauris venenatis, pede sed aliquet vehicula, lectus tellus  pulvinar neque, non cursus sem nisi vel augue. sed aliquet vehicula, lectus tellus pulvinar neque, non cursus sem nisi vel augue. ipsum  primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras  ornare mattis nunc. Mauris venenatis, pede sed aliquet vehicula, lectus tellus pulvinar neque, non cursus sem nisi vel augue. sed aliquet  vehicula, lectus tellus pulvinar neque, non cursus sem nisi vel augue</p>
+					</div>
+				</aside>
+			</section>
+		</div>
+	</div>
+</div>
+<footer>
+	<div class="eu-wrapper">
+		<div class="eu-wbox">
+			<p>© Company 2011 &ndash; Layout based on <a href="http://www.eurekathemes.com">EUREKA THEMES</a></p>
+		</div>
+	</div>
+</footer>
+<!-- full skip link functionality in webkit browsers -->
+<script src="js/core/js/yaml-focusfix.js"></script>
+</body>
+</html>
